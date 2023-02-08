da66a15164b1982baee9d57842df64992bd10762
Brahma Keerthi <brahmakeerthihs008@gmail.com>
2023-02-08 20:04:21 +0530

    TCP Program
    Co-authored-by: Akash N <akashakku2202@gmail.com>

--- 5_TCP/Client.java
+++ 5_TCP/Client.java
@@ -3,15 +3,15 @@ import java.io.*;
 
 public class Client {
     public static void main(String args[]) throws Exception{
-        Socket sock=new Socket("127.0.0.1",4000);
+        Socket socket =new Socket("127.0.0.1",4000);
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter Filename : ");
         String fname=br.readLine();
-        OutputStream oStream=sock.getOutputStream();
+        OutputStream oStream=socket.getOutputStream();
         PrintWriter pWrite=new PrintWriter(oStream,true);
         pWrite.println(fname);
 
-        InputStream istream=sock.getInputStream();
+        InputStream istream=socket.getInputStream();
         BufferedReader br2=new BufferedReader(new InputStreamReader(istream));
         String str;
         while((str=br2.readLine())!=null){
