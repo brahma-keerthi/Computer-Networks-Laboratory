bd76d2c680b95cc6869d06f8113482a384fc7739
Brahma Keerthi <brahmakeerthihs008@gmail.com>
2023-02-08 20:00:33 +0530

    TCP Program

---
+++ 5_TCP/Client.java
@@ -0,0 +1,22 @@
+import java.net.*;
+import java.io.*;
+
+
+public class Client {
+    public static void main(String args[]) throws Exception{
+        Socket sock=new Socket("127.0.0.1",4000);
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter Filename : ");
+        String fname=br.readLine();
+        OutputStream oStream=sock.getOutputStream();
+        PrintWriter pWrite=new PrintWriter(oStream,true);
+        pWrite.println(fname);
+
+        InputStream istream=sock.getInputStream();
+        BufferedReader br2=new BufferedReader(new InputStreamReader(istream));
+        String str;
+        while((str=br2.readLine())!=null){
+            System.out.println(str);
+        }
+    }
+}
---
+++ 5_TCP/Server.java
@@ -0,0 +1,30 @@
+import java.net.*;
+import java.io.*;
+
+public class Server{
+    public static void main(String args[]) throws Exception{
+        ServerSocket sersoc = new ServerSocket(4000);
+        System.out.println("Server Connected, Waiting for Client");
+        Socket sock = sersoc.accept();
+        System.out.println("Connection Successfull, Waiting for filename :)");
+        InputStream istream = sock.getInputStream();
+        BufferedReader readBuffer = new BufferedReader(new InputStreamReader(istream));
+        String fname = readBuffer.readLine();
+        OutputStream ostream = sock.getOutputStream();
+        PrintWriter pwrite = new PrintWriter(ostream, true);
+
+        try {
+            BufferedReader contentRead = new BufferedReader(new FileReader(fname));
+            String str;
+            while ((str = contentRead.readLine()) != null ){
+                pwrite.println(str);
+            }
+        }
+        catch ( Exception e ){
+            pwrite.println("File name does not exists");
+        }
+        finally{
+            System.out.println("Closing Connection");
+        }
+    }
+}
